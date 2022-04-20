d8c8f9631782a151f0ba84cf3bda5e772f811b48
Codexive_zech <zechariahhounwanou@gmail.com>
2022-04-20 21:58:14 +0100

    implemented the countdown for the days, hours, minutes and seconds
    subtrated future date fro today date converted them to milliseconds so it will be easy to calculate days, hours, minutes & seconds, then acculmulated them into an array displayed it on the HTML, set a countdown interval and a clear countdown interval when the date is passed

--- 15-countdown-timer/countdown.js
+++ 15-countdown-timer/countdown.js
@@ -28,7 +28,7 @@ const weekdays = [
 // Declaring and Defining actions and effects
 const giveaway = document.querySelector('.giveaway');
 
-// displaying the future date to end countdown
+// displaying the future date to end countdown.
 const futureDate = new Date(2022, 9, 17, 23, 30, 00);
 
 // Future date extraction begin
@@ -40,7 +40,7 @@ const getHour = futureDate.getHours();
 const getMinute = futureDate.getMinutes();
 // getting the exact Date the countdown end
 const getDate = futureDate.getDate();
-// getting the exact Month the countdown end
+// getting the exact Month the countdown end.
 let getMonth = futureDate.getMonth();
 // reassign the value and check the month array to get the index
 getMonth = months[getMonth];
