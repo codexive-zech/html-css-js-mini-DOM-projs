71711df9eaf9c3e98606b3e572202e183d4fbca8
Codexive_zech <zechariahhounwanou@gmail.com>
2022-04-21 14:47:56 +0100

    Implemented the alert display func
    dynamically display text and class in the HTML and remove it after some seconds using setTimeout func inside of the display func

--- 11-grocery-bud/js/app.js
+++ 11-grocery-bud/js/app.js
@@ -26,11 +26,32 @@ function addGroceryItem(e) {
   const id = new Date().getTime().toString();
   //   Checking to see if as long an the groceryValue(groceryInput) is not empty and editFlag (mode) is false
   if (groceryValue !== '' && editFlag === false) {
-    console.log('Adding New Items');
     // Checking to see if as long an the groceryValue(groceryInput) is not empty and editFlag (mode) is true
+    displayAlert('Grocery Item Added Successfully', 'success');
   } else if (groceryValue !== '' && editFlag === true) {
     console.log('Editing Item');
   } else {
-    console.log('empty');
+    //  invoking the display Alert fun when the submit event takes place
+    displayAlert('Empty', 'danger');
   }
+}
+
+// the display Function for alert
+function displayAlert(text, bgAction) {
+  // dynamically displaying text in the HTML
+  alert.textContent = text;
+  // dynamically adding the class for the alert bgColor
+  alert.classList.add(`alert-${bgAction}`);
+
+  //   setting a timeout fun so that it can be gone after the specified duration
+  setTimeout(
+    function () {
+      //   getting back an empty string into the HTML
+      alert.textContent = '';
+      // removing the dynamically added class for the alert bgColor
+      alert.classList.remove(`alert-${bgAction}`);
+    },
+    //   setting the specific duration before it goes out
+    2000
+  );
 }
